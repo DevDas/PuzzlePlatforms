1b076ecfd7b56ff86f32df9c58b110136de30feb
DevDas <55701713+DevDas@users.noreply.github.com>
2020-02-20 20:45:57 +0530

    Replicate And Authorities
    if !HasAuthority() then The Code Will Run On Client Only

--- PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -1,9 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
-
-
 
 AMovingPlatform::AMovingPlatform()
 {
@@ -12,11 +9,22 @@ AMovingPlatform::AMovingPlatform()
 	SetMobility(EComponentMobility::Movable);
 }
 
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority())
+	if (!HasAuthority()) // Not on Server == client
 	{
 		auto Location = GetActorLocation();
 		Location += FVector(Speed * DeltaTime, 0, 0);
