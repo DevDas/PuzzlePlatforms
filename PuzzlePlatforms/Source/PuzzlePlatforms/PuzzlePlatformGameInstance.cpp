485e22247d5d8d82203976c268b8aec84b6aa090
DevDas <55701713+DevDas@users.noreply.github.com>
2020-03-20 04:22:42 +0530

    Hosting And Joining Over Console Command Completed

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformGameInstance.cpp
@@ -3,6 +3,8 @@
 
 #include "PuzzlePlatformGameInstance.h"
 #include "Engine/Engine.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
 UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
 {
@@ -19,7 +21,12 @@ void UPuzzlePlatformGameInstance::Host()
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 	// 0 means, each time through we will update the existing message instead 
-	Engine->AddOnScreenDebugMessage(0, 2, FColor::Blue, TEXT("Hosting"));
+	Engine->AddOnScreenDebugMessage(0, 10, FColor::Blue, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformGameInstance::Join(const FString& Address)
@@ -27,5 +34,10 @@ void UPuzzlePlatformGameInstance::Join(const FString& Address)
 	UEngine* Engine = GetEngine();
 	if (!ensure(Engine != nullptr)) return;
 	// 0 means, each time through we will update the existing message instead 
-	Engine->AddOnScreenDebugMessage(0, 2, FColor::Blue, FString::Printf(TEXT("Joining %s"), *Address));
+	Engine->AddOnScreenDebugMessage(0, 10, FColor::Blue, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
